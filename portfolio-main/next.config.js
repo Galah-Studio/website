1b4383d0b7da793eb058e74faa93e877fcbc9844
Galah-Studio <hamid@galah.tv>
2024-07-27 18:52:54 +0300

    Update next.config.js

--- portfolio-main/next.config.js
+++ portfolio-main/next.config.js
@@ -1,14 +1,9 @@
 const prod = process.env.NODE_ENV === "production";
-
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: prod ? false : true,
+  disable: !prod
 });
 
-const nextConfig = {};
-
-module.exports = withPWA(nextConfig);
- 
+module.exports = withPWA({});
