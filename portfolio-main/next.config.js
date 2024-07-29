3eda43572bd4dd77544dc8a2d0ee80e772006ab3
Galah-Studio <hamid@galah.tv>
2024-07-29 22:13:33 +0300

    Delete portfolio-main directory

--- portfolio-main/next.config.js
+++
@@ -1,9 +0,0 @@
-const prod = process.env.NODE_ENV === "production";
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: !prod
-});
-
-module.exports = withPWA({});
